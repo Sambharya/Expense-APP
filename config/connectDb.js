b5ee9d80e1ba85738fca8aba8e4b0d5cfcf5d19a
Sambharya <abhisheksambharya05@gmail.com>
2024-07-21 13:32:28 +0530

    expense-app

--- config/connectDb.js
+++ config/connectDb.js
@@ -4,17 +4,16 @@ const dotenv = require("dotenv");
 // Load environment variables
 dotenv.config();
 
+// Set `strictQuery` option before connecting to MongoDB
+mongoose.set('strictQuery', false);
+
 const connectDb = async () => {
   try {
     // Connect to MongoDB with updated options
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // Remove deprecated options
     });
-
-    // Set `strictQuery` option to suppress warning
-    mongoose.set('strictQuery', false);
 
     console.log(`MongoDB Connected: ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
